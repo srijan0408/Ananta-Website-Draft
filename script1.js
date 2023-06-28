9774e6e18457d62769ff152599319f1402793c93
Sriyukk <122951216+Sriyukk@users.noreply.github.com>
2023-06-28 20:26:21 +0530

    Animation commit

--- script1.js
+++ script1.js
@@ -14,3 +14,36 @@ function chnageReiew(){
     buttons[i].classList.add("active-btn");
     texts[i].classList.add("active-text");
 }
+
+
+/*JS For Animmation */
+
+const observer =  new IntersectionObserver((entries) =>{
+    entries.forEach((entry)=>{
+        console.log(entry);
+        if(entry.isIntersecting){
+            entry.target.classList.add('show');
+        }
+        else{
+            entry.target.classList.remove('show')
+        }
+    });
+});
+
+const hiddenElement = document.querySelectorAll('.hidden');
+hiddenElement.forEach((el) => observer.observe(el));
+
+const observer1 =  new IntersectionObserver((entries) =>{
+    entries.forEach((entry)=>{
+        console.log(entry);
+        if(entry.isIntersecting){
+            entry.target.classList.add('show1');
+        }
+        else{
+            entry.target.classList.remove('show1')
+        }
+    });
+});
+
+const hiddenElement1 = document.querySelectorAll('.hidden1');
+hiddenElement1.forEach((el) => observer1.observe(el));
